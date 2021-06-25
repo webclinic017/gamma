8b6a4c062fa6d5fb6cf16fccb5b8eacd6c6daafc
David Clevenger <dclevenger00@gmail.com>
2021-06-24 23:08:54 -0400

    rust(2).

--- src/strategy/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod teststrat;
-
-use std::collections::HashMap;
-
-use crate::datasource::DataSource;
-
-pub trait Strategy {
-    fn process<T: DataSource>(&mut self, data: &mut T) -> HashMap<&'static str, f64>;
-}
